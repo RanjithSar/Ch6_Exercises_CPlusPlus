fc01f112c7adec989ae932bdbc058c6b7a5aecee
Ranjith <itssrranjith@gmail.com>
2025-05-27 18:57:24 -0400

    wrote attributes, default constructor, and user constructor

--- employee_class.cpp
+++ employee_class.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class employee{};
+class employee{
+	
+	private:
+		int emp_num;
+		float comp;
+		
+	public:
+		employee(): emp_num(0), comp(0.0)
+		{}
+		employee(int e_num, float comp){
+			this->emp_num = e_num;
+			this->comp = comp;
+		}
+};
 
 int main(){}
