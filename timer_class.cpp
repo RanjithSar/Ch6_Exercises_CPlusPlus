80b47b0b01d27b4a9ec7a829fcb904230cbcd862
Ranjith <itssrranjith@gmail.com>
2025-05-26 21:48:00 -0400

    wrote the add_two_times method to add two time objects together

--- timer_class.cpp
+++ timer_class.cpp
@@ -21,6 +21,23 @@ class time{
 			printf("%02d:%02d:%02d\n", this->hours, this->minutes, this->seconds);
 		}
 		
-		
+		void add_two_times(time t1, time t2){
+			this->seconds += (t1.hours * 3600) + (t1.minutes * 60) + t1.seconds;
+			this->seconds += (t2.hours * 3600) + (t2.minutes * 60) + t2.seconds;
+			
+			while (this->seconds > 60){
+				this->minutes++;
+				this->seconds -= 60;
+			}
+			
+			while (this->minutes > 60){
+				this->hours++;
+				this->minutes -= 60;
+			}
+			
+			if (this->hours >= 12){
+				this->hours %= 12;
+			}
+		}
 	
 };
