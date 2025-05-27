603514269d147766c937a43a144e35ad52dd14c0
Ranjith <itssrranjith@gmail.com>
2025-05-26 20:26:56 -0400

    created main function with two initialized Int's and one uninitialized Int, and the two initialized ones added to the uninitialized one.

--- Int_class.cpp
+++ Int_class.cpp
@@ -18,3 +18,23 @@ class Int{
 			this->value += other.value;
 		}
 };
+
+int main(){
+	
+	Int val1;
+	Int val2(34);
+	Int val3(23);
+	
+	val1.display();
+	val2.display();
+	val3.display();
+	
+	cout << "============" << endl;
+	
+	val1.add_Int(val2);
+	val1.add_int(val3);
+	
+	val1.display();
+	
+	return 0;
+}
