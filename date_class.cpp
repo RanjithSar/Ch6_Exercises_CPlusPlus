0b3d612628875e0fba24fe80dafd4f673403d8f4
Ranjith <itssrranjith@gmail.com>
2025-05-28 00:09:51 -0400

    defined attributes and wrote both constructors

--- date_class.cpp
+++ date_class.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
-class date{};
+class date{
+	
+	private:
+		int month;
+		int day;
+		int year;
+		
+	public:
+		date(): month(0), day(0), year(0)
+		{}
+		date(int month, int day, int year){
+			this->month = month;
+			this->day = day;
+			this->year = year;
+		}
+	
+};
 
 int main(){}
