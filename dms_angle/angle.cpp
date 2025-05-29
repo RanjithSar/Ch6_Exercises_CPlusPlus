68e25bbdb8a5c226cfa1ed8b7722d2c72e5686d2
Ranjith <itssrranjith@gmail.com>
2025-05-29 19:04:39 -0400

    refined output displaying code and main() test code to show angles correctly. Also optimized showangle for later point class for latitude and longitude.

--- dms_angle/angle.cpp
+++ dms_angle/angle.cpp
@@ -3,7 +3,9 @@
 using namespace std;
 
 angle::angle(): degrees(0), minutes(0), seconds(0)
-{}
+{
+	this->direction = 'X';
+}
 
 angle::angle(int deg, int min, int sec, char dir){
 	this->degrees = deg;
--- dms_angle/main.cpp
+++ dms_angle/main.cpp
@@ -5,8 +5,19 @@ using namespace std;
 int main(){
 	
 	angle myAngle;
+	angle otherAngle(96,21,12,'W');
+	
+	myAngle.showangle();
+	cout << "\n";
+	otherAngle.showangle();
+	cout << "\n";
+	
+	cout << "===================" << endl;
+	
 	myAngle.getangle();
 	
 	myAngle.showangle();
-	
+	cout << "\n";
+	otherAngle.showangle();
+	cout << "\n";
 }
