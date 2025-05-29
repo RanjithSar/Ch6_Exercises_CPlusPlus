0b5afa520502c931134382466bc111ac97fb5bfa
Ranjith <itssrranjith@gmail.com>
2025-05-28 22:16:41 -0400

    documented employee.h file.

--- employee_class_extension/employee.h
+++ employee_class_extension/employee.h
@@ -1,9 +1,24 @@
+/*
+* Includes the employee class template.
+* This header file declares all the 
+* methods and attributes of the employee class.
+*/
+
+/*
+Include the date class to store date of first
+employment.
+*/
 #include "date.h"
 
 class employee{
 	
 	private:
 	
+		/*
+		Static enumeration pre-defining the roles
+		an employee can take.
+		NOTE: enumerations are static by default.
+		*/
 		enum etype{
 			NONE = 0,
 			labourer = 1,
@@ -12,18 +27,29 @@ class employee{
 			accountant = 4
 		};
 	
+		// For ID and yearly compensation.
 		int emp_id;
 		float comp;
 		
+		// Date of first employment
 		date first_emp;
+		
+		/*
+		Variable of type mytype enumeration to store
+		the employee's position as an integer.
+		This integer is mapped to a position in the enumeration.
+		*/
 		etype mytype;
 		
 	public:
 	
+		// Constructor
 		employee();
 		
+		// Asks user to initialize.
 		void getemploy();
 		
+		// Shows employee data to terminal.
 		void showemploy() const;
 		
 };
