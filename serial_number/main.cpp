1fbfe848648780e2b8a0af7a0ad100014e494993
Ranjith <itssrranjith@gmail.com>
2025-05-30 17:06:14 -0400

    added arrays and pointer arrays of serial_number objects to test out the functionality of the class.

--- serial_number/main.cpp
+++ serial_number/main.cpp
@@ -9,6 +9,25 @@ int main(){
 	s2.showSerialNo();
 	s3.showSerialNo();
 	
+	serial_number* my_serial_arr[10];
+	serial_number my_other_serial_arr[10];
+	
+	for (int i = 0; i < 10; i++){
+		my_serial_arr[i] = new serial_number;
+		my_serial_arr[i]->showSerialNo();
+		my_other_serial_arr[i].showSerialNo();
+	}
+	
+	serial_number sAfterLoop;
+	sAfterLoop.showSerialNo();
+	
+	for (int i = 0; i < 10; i++){
+		delete my_serial_arr[i];
+		my_serial_arr[i] = nullptr;
+	}
+	
+	//delete[] my_serial_arr;
+	
 	return 0;
 	
 }
