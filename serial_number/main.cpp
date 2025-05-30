e9cd38720fc56535b75d0205330b9f050ef9075a
Ranjith <itssrranjith@gmail.com>
2025-05-30 16:43:54 -0400

    created three serial_number objects to test out functionality.

---
+++ serial_number/main.cpp
@@ -0,0 +1,14 @@
+#include "serial_number.h"
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	serial_number s1, s2, s3;
+	s1.showSerialNo();
+	s2.showSerialNo();
+	s3.showSerialNo();
+	
+	return 0;
+	
+}
