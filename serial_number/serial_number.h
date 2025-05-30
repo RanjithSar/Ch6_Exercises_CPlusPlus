ef70fca11e838a4be5750d5693ae650608bc1a5b
Ranjith <itssrranjith@gmail.com>
2025-05-30 17:21:29 -0400

    documented header file.

--- serial_number/serial_number.h
+++ serial_number/serial_number.h
@@ -1,11 +1,19 @@
 class serial_number{
 	
 	private:
+	
+		// Stores the number of all instances created.
 		static unsigned int count;
+		
+		// Stores each individual object's serial number.
 		unsigned int serial_no;
 		
 	public:
+	
+		// Constructor to initialize each object.
 		serial_number();
+		
+		// To show the object's serial number to the terminal.
 		void showSerialNo() const;
 	
 };
